66b4b0d8061116dc0675fdb267579e7c21ac0877
Telegram Farming Build <telegramfarmingapp@users.noreply.github.com>
2026-08-11 13:37:31 +0330

    deploy: build-0.0.6

---
+++ assets/start-mocking-B9MIC2M3.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/browser-DDlE3fiA.js","assets/index-jtGsKOZP.js","assets/index-vVHW-wuF.css"])))=>i.map(i=>d[i]);
+import{c as e,l as t}from"./index-jtGsKOZP.js";function n(e){return e===`mock`}async function r(r=t.VITE_API_MODE){if(!n(r))return;let{worker:i}=await e(async()=>{let{worker:e}=await import(`./browser-DDlE3fiA.js`);return{worker:e}},__vite__mapDeps([0,1,2]));await i.start({onUnhandledRequest:`error`,serviceWorker:{url:`/telegramfarmingapp/mockServiceWorker.js`}})}export{r as startMocking};
